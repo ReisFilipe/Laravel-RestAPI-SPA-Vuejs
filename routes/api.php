94d7b3f323db968273bb36118bfd90d967b4faa8
Filipe Reis <filipereisaranha@gmail.com>
2022-10-09 22:48:23 -0300

    Login sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\CidadeController;
 use App\Http\Controllers\Api\EstadoController;
+use App\Http\Controllers\API\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,22 +17,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->group(function(){
+Route::prefix('v1')->middleware('auth:sanctum')->group(function(){
 
     Route::namespace('estado.')->group(function(){
 
         Route::get('estado/{id}/cidades', [EstadoController::class, 'cidade'])->name('cidade');
-
         Route::get('estado/', [EstadoController::class, 'index'])->name('index');
         Route::get('estado/{id}', [EstadoController::class, 'show'])->name('show');
         Route::post('estado/', [EstadoController::class, 'store'])->name('store');
         Route::put('estado/{id}', [EstadoController::class, 'update'])->name('update');
         Route::delete('estado/{id}', [EstadoController::class, 'delete'])->name('delete');
+    });
 
+    Route::namespace('cidade.')->group(function(){
+
+        Route::get('cidade/', [CidadeController::class, 'index'])->name('index');
+        Route::get('cidade/{id}', [CidadeController::class, 'show'])->name('show');
+        Route::post('cidade/', [CidadeController::class, 'store'])->name('store');
+        Route::put('cidade/{id}', [CidadeController::class, 'update'])->name('update');
+        Route::delete('cidade/{id}', [CidadeController::class, 'delete'])->name('delete');
     });
 
 });
